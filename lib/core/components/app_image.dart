fd06485391e492f7d7c9ce6af10248a6ef5485be
Fekry <me031230@gmail.com>
2026-05-18 05:56:19 +0300

    refactor(AppImage): unify rendering logic into a smart factory, enforce cover fit for circles, and add empty path safeguard

--- lib/core/components/app_image.dart
+++ lib/core/components/app_image.dart
@@ -1,45 +1,75 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:gap/gap.dart';
+import 'package:lottie/lottie.dart';
 
 class AppImage extends StatelessWidget {
+  final String image;
+  final double? height, width;
+  final bool isCircle;
+  final Color? color;
+  final double? bottomSpace;
+  final BoxFit fit;
+
   const AppImage({
     super.key,
     required this.image,
     this.height,
     this.width,
+    this.isCircle = false,
     this.color,
-    this.fit,
+    this.fit = BoxFit.scaleDown,
+    this.bottomSpace,
   });
-  final String image;
-  final double? height, width;
-  final Color? color;
-  final BoxFit? fit;
 
   @override
   Widget build(BuildContext context) {
-    if (image.endsWith("svg")) {
-      return SvgPicture.asset(
-        "assets/icons/$image",
-        color: color,
-        height: height,
-        width: width,
-        fit: fit ?? BoxFit.scaleDown,
-      );
-    } else if (image.startsWith("http")) {
-      return Image.network(
-        image,
-        color: color,
-        height: height,
-        width: width,
-        fit: fit ?? BoxFit.scaleDown,
-      );
-    }
-    return Image.asset(
-      "assets/icons/$image",
-      color: color,
-      height: height,
-      width: width,
-      fit: fit ?? BoxFit.scaleDown,
+    final myfit = isCircle ? BoxFit.cover : fit;
+
+    return Padding(
+      padding: bottomSpace != null
+          ? EdgeInsets.only(bottom: bottomSpace!)
+          : EdgeInsets.zero,
+      child: Builder(
+        builder: (context) {
+          Widget child;
+          if (image.isEmpty) return SizedBox.shrink();
+          if (image.toLowerCase().endsWith("svg")) {
+            child = SvgPicture.asset(
+              "assets/icons/$image",
+              color: color,
+              height: height,
+              width: width,
+              fit: myfit,
+            );
+          } else if (image.startsWith("http")) {
+            child = Image.network(
+              image,
+              height: height,
+              width: width,
+              color: color,
+              fit: myfit,
+            );
+          } else if (image.endsWith("json")) {
+            child = Lottie.asset(
+              "assets/lotties/$image",
+              height: height,
+              width: width,
+              fit: myfit,
+            );
+          } else {
+            child = Image.asset(
+              "assets/images/$image",
+              color: color,
+              height: height,
+              width: width,
+              fit: myfit,
+            );
+          }
+
+          return isCircle ? ClipOval(child: child) : child;
+        },
+      ),
     );
   }
 }

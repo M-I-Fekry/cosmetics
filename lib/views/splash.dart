3f6cc8fdec4d3f209fe8c8338e2a296cdb02fc85
Fekry <me031230@gmail.com>
2026-06-02 14:46:35 +0300

    feat(splash): implement routing logic based on auth and first-time state

--- lib/views/splash.dart
+++ lib/views/splash.dart
@@ -3,6 +3,7 @@ import 'package:cosmetics/core/components/app_image.dart';
 import 'package:cosmetics/core/logic/cache_helper.dart';
 import 'package:cosmetics/core/logic/helper_methods.dart';
 import 'package:cosmetics/views/auth/login.dart';
+import 'package:cosmetics/views/home/view.dart';
 import 'package:cosmetics/views/on_boarding.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -18,7 +19,15 @@ class _SplashviewState extends State<Splashview> {
   @override
   void initState() {
     super.initState();
-    goTo(CacheHelper.isFirstTime? OnBoardingView():LoginView(), delayInSeconds: 2, preventPop: true);
+    goTo(
+      CacheHelper.isFirstTime
+          ? OnBoardingView()
+          : CacheHelper.isAuth
+          ? HomeView()
+          : LoginView(),
+      delayInSeconds: 2,
+      preventPop: true,
+    );
   }
 
   @override
@@ -26,26 +35,26 @@ class _SplashviewState extends State<Splashview> {
     return Scaffold(
       body: Center(
         child: Column(
-  mainAxisSize: MainAxisSize.min,
-  children: [
-    ZoomIn(
-      child: AppImage(
-        image: "logo.png",
-        height: 200.h,
-        width: 200.w,
-        bottomSpace: 16.h,
-      ),
-    ),
-    FadeInUp(
-      delay: const Duration(milliseconds: 600),
-      child: AppImage(
-        image: "splash_logo2.png", 
-        height: 46.h, 
-        width: 120.w,
-      ),
-    ),
-  ],
-)
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ZoomIn(
+              child: AppImage(
+                image: "logo.png",
+                height: 200.h,
+                width: 200.w,
+                bottomSpace: 16.h,
+              ),
+            ),
+            FadeInUp(
+              delay: const Duration(milliseconds: 600),
+              child: AppImage(
+                image: "splash_logo2.png",
+                height: 46.h,
+                width: 120.w,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
